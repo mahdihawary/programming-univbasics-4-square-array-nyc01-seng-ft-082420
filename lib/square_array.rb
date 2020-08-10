e8edd6d070e5e944039ddd320fde3745cc229a4b
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-10 02:34:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(numbers)
  new_array[]
  count=0
   while count < number.length do
-     new_array.push(numbers[count]**)
-      count+=1
+    # new_array.push(numbers[count]**)
+    count+=1
    end 
   
 end
